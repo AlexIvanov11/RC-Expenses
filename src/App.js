a77885e7ea37bdc4a025d90bf403173c54220dd7
Alex <Aleksandr.Ivanov@t-systems.com>
2021-08-22 23:40:42 +0300

    added form and sample listener

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Expenses } from "./components/Expenses/Expenses";
+import { NewExpense } from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -24,7 +25,7 @@ const App = () => {
   ];
   return (
     <div className="App">
-      <h1>Expense tracker</h1>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
