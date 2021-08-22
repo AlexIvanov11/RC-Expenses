07a4502dd497495ba58d485d026143707f76a939
Alex <Aleksandr.Ivanov@t-systems.com>
2021-08-22 21:28:43 +0300

    basic project markup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import ExpenseItem from "./components/ExpenseItem";
+import { Expenses } from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
+  const expenses = [
+    {
+      id: "1",
+      title: "Car insurance",
+      amount: 322,
+      date: new Date(2021, 5, 6),
+    },
+    {
+      id: "2",
+      title: "Health insurance",
+      amount: 4343,
+      date: new Date(2021, 3, 6),
+    },
+    { id: "3", title: "Tyres", amount: 55, date: new Date(2021, 5, 17) },
+    {
+      id: "4",
+      title: "Intel shares",
+      amount: 567,
+      date: new Date(2021, 5, 11),
+    },
+  ];
   return (
     <div className="App">
       <h1>Expense tracker</h1>
-      <ExpenseItem />
+      <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
